fbda0d031d975d8bc2d9cdb17ab7b12aece7ecde
muugii9902 <muugii9902@gmail.com>
2024-07-21 22:00:19 +0800

    home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
-// import Image from "next/image";
-// import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Navbar from "@/components/navbar";
 import Sheet01 from "@/components/sheet01";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
